24ad0e394332057833a55a0104c855843d1e55f0
bala44858 <bala44858@gmail.com>
2022-11-05 17:54:55 +0530

    tic tac toe app  first commit

---
+++ app/src/main/java/com/example/tiktactoe/flashScrean.kt
@@ -0,0 +1,11 @@
+package com.example.tiktactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class flashScrean : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_screan)
+    }
+}
