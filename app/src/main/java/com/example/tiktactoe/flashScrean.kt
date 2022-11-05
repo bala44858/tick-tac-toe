0b8f8e175e18d18336e854e5581e2fc0f416d048
bala44858 <bala44858@gmail.com>
2022-11-05 18:29:23 +0530

    flash screan done

--- app/src/main/java/com/example/tiktactoe/flashScrean.kt
+++ app/src/main/java/com/example/tiktactoe/flashScrean.kt
@@ -1,11 +1,25 @@
 package com.example.tiktactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class flashScrean : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_screan)
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        },3000)
+
+
     }
 }
